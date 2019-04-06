d71aba2299cd8046df1cbf3f3f0dc16e49ea9e51
Eldar Kalachev <eldarkalachev@Eldars-MBP.Dlink>
2019-04-06 17:42:43 +0300

    added the function findAllDivisors

--- src/GreatestCommonDivisor.java
+++ src/GreatestCommonDivisor.java
@@ -1,5 +1,8 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class GreatestCommonDivisor {
-    public int GCD(int a, int b) {
+    public static int GCD(int a, int b) {
         while (b != 0) {
             int reminder = a % b;
             a = b;
@@ -7,5 +10,22 @@ public class GreatestCommonDivisor {
         }
 
         return a;
+    }
+
+    public static int[] findAllDivisors(int n) {
+        List<Integer> divisors = new ArrayList<Integer>();
+
+        for (int i = 1; i <= Math.sqrt(n); i++) {
+            if(n%i == 0) {
+                int divisor = n/i;
+                divisors.add(divisor);
+
+                if (divisor != i) {
+                    divisors.add(i);
+                }
+            }
+        }
+
+        return divisors.stream().mapToInt(x -> x).toArray();
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -11,7 +10,7 @@ public class Main {
         do {
             sc.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-            List<Integer> arr = FindPrimeNumbers.findPrimeNumbers(sc.nextInt());
+            int[] arr = GreatestCommonDivisor.findAllDivisors(sc.nextInt());
 
             for (int e : arr) {
                 System.out.printf("%s ", e);
