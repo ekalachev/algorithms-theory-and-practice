fd1c8b6fa78049326ca79c9cc18d9b9f7a2ab9ba
Eldar Kalachev <rusgunter@gmail.com>
2019-05-15 00:29:27 +0300

    comment fix

--- src/hackerrankAlgorithms/arrayManipulation/ArrayManipulation.java
+++ src/hackerrankAlgorithms/arrayManipulation/ArrayManipulation.java
@@ -3,12 +3,13 @@
  	element between two given indices, inclusive. Once all operations have been performed, return the maximum value in your array.
 	For example, the length of your array of zeros n = 10. Your list of queries is as follows:
 	a b k
-    1 5 3
-    4 8 7
-    6 9 1
+	1 5 3
+	4 8 7
+	6 9 1
     
     Add the values of 'k' between the indices 'a' and 'b' inclusive:
-    index->	 1 2 3  4  5 6 7 8 9 10
+	index->	 
+			 1 2 3  4  5 6 7 8 9 10
 			[0,0,0, 0, 0,0,0,0,0, 0]
 			[3,3,3, 3, 3,0,0,0,0, 0]
 			[3,3,3,10,10,7,7,7,0, 0]
