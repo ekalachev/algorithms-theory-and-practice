d7756fa1435a627e7c97d62e819e14649959c8e6
Eldar Kalachev <rusgunter@gmail.com>
2019-05-15 07:54:07 +0300

    Hackerrunk: Array Manipulation added second approach with time complexity O(m log m)

--- src/hackerrankAlgorithms/arrayManipulation/ArrayManipulation.java
+++ src/hackerrankAlgorithms/arrayManipulation/ArrayManipulation.java
@@ -15,31 +15,85 @@
 			[3,3,3,10,10,7,7,7,0, 0]
 			[3,3,3,10,10,8,8,8,1, 0]
 	The largest value is 10 after all operations are performed.
+	
+	Optimal SOLUTION O(n):
+	Given a range[a, b] and a value 'k' we need to add 'k' to all the numbers whose indices are in the range from [a, b].
+	We can do an O(1) update by adding 'k' to index 'a' and add '-k' to index 'b+1'.
+	Doing this kind of update, the 'i'th number in the array will be prefix sum of array from index 1 to i because we are
+	adding 'k' to the value at index 'a' and subtracting 'k' from the value at index 'b+1' and taking prefix sum will give
+	us the actual value for each index after 'm' operations .
+	So, we can do all 'm' updates in O(m) time. Now we have to check the largest number in the original array.
+	i.e. the index i such that prefix sum attains the maximum value.
+	We can calculate all prefix sums as well as maximum prefix sum in O(n) time which will execute in time.
+	
+	Optimal SOLUTION O(m log m):
+	This can be further optimized to run in O(m log m) time because we have to check the value of prefix sum
+	at only '2 * m' indices. i.e. 'a' and 'b' values of all the updates.
+	We have, in total 'm' queries and each query has a range [a, b] which needs to be updated.
+	So, in total we have '2 * m' indices.
+	For each query, we can insert both 'a', 'k' and 'b + 1', '-k' in an array and sort the array.
+	Now, we have to just take the prefix sum of the array and find the maximum element which will be our answer.
  */
 package hackerrankAlgorithms.arrayManipulation;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import common.Pair;
+
 public class ArrayManipulation {
-    public long arrayManipulation(int n, int[][] queries) {
-        long [] arr = new long[n];
+	// time complexity: O(n)
+	public long arrayManipulation(int n, int[][] queries) {
+		int m = queries.length;
+		long[] arr = new long[n];
 
-        for(int i = 0; i < queries.length; i++) {
-            int from = queries[i][0] - 1;
-            int to = queries[i][1];
-            int num = queries[i][2];
+		for (int i = 0; i < m; i++) {
+			int from = queries[i][0] - 1;
+			int to = queries[i][1];
+			int num = queries[i][2];
 
-            arr[from] += num;
-            if (to < n)
-                arr[to] -= num;
-        }
+			arr[from] += num;
+			if (to < n)
+				arr[to] -= num;
+		}
 
-        long max = 0, sum = 0;
+		long max = 0, sum = 0;
 
-        for(long i: arr){
-            sum += i;
-            if(max < sum)
-                max = sum;
-        }
+		for (long i : arr) {
+			sum += i;
+			if (max < sum)
+				max = sum;
+		}
 
-        return max;
-    }
+		return max;
+	}
+
+	// time complexity O(m log m)
+	public long arrayManipulation2(int n, int[][] queries) {
+		int m = queries.length;
+
+		List<Pair<Integer, Integer>> arr = new ArrayList<>();
+
+		for (int i = 0; i < m; i++) {
+			int from = queries[i][0] - 1;
+			int to = queries[i][1];
+			int num = queries[i][2];
+
+			arr.add(new Pair<Integer, Integer>(from, num));
+			arr.add(new Pair<Integer, Integer>(to, -num));
+		}
+
+		Collections.sort(arr, Comparator.comparing(Pair<Integer, Integer>::getKey));
+
+		long max = 0, sum = 0;
+
+		for (int i = 0; i < 2 * m; i++) {
+			sum += (long) arr.get(i).getValue();
+			max = Math.max(sum, max);
+		}
+
+		return max;
+	}
 }
--- src/hackerrankAlgorithms/arrayManipulation/ArrayManipulationTest.java
+++ src/hackerrankAlgorithms/arrayManipulation/ArrayManipulationTest.java
@@ -14,7 +14,7 @@ public class ArrayManipulationTest {
 	}
 
 	@Test
-	public void test() {
+	public void given_gaeries_of_ranges_with_sums_When_arrayManipulation_then_returns_max_sum() {
 		// Given
 		int m = 3;
 		int[][] queries = new int[m][3];
@@ -34,6 +34,33 @@ public class ArrayManipulationTest {
 		
 		// When
 		long max = am.arrayManipulation(5, queries);
+		
+		
+		// Then
+		assertEquals(expectedMax, max);
+	}
+	
+	@Test
+	public void given_gaeries_of_ranges_with_sums_When_arrayManipulation2_then_returns_max_sum() {
+		// Given
+		int m = 3;
+		int[][] queries = new int[m][3];
+		queries[0][0] = 1;
+		queries[0][1] = 2;
+		queries[0][2] = 100;
+		
+		queries[1][0] = 2;
+		queries[1][1] = 5;
+		queries[1][2] = 100;
+		
+		queries[2][0] = 3;
+		queries[2][1] = 4;
+		queries[2][2] = 100;
+		
+		long expectedMax = 200;
+		
+		// When
+		long max = am.arrayManipulation2(5, queries);
 		
 		
 		// Then
